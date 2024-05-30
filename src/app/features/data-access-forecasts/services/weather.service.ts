4e290b302fabe3eb3ffe4dde59fc745bd0944ff6
Marcin Gawski <marcin.gawski@gmail.com>
2024-05-30 12:36:36 +0200

    Kubernetes deployments configured.

--- src/app/features/data-access-forecasts/services/weather.service.ts
+++ src/app/features/data-access-forecasts/services/weather.service.ts
@@ -5,7 +5,7 @@ import { ConditionsAndZip, CurrentConditions, Forecast } from '@core/types';
 
 @Injectable({ providedIn: 'root' })
 export class WeatherService {
-  static URL = 'http://api.openweathermap.org/data/2.5';
+  static URL = 'https://api.openweathermap.org/data/2.5';
   static APPID = '5a4b2d457ecbef9eb2a71e480b947604';
   static ICON_URL =
     'https://raw.githubusercontent.com/udacity/Sunshine-Version-2/sunshine_master/app/src/main/res/drawable-hdpi/';
