bc4e74f8a65bdad299ab4d8025526b90abd5da89
Marcin Gawski <marcin.gawski@gmail.com>
2024-05-30 13:02:07 +0200

    Installed custom fonts.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '2rem',
+    },
+    extend: {
+      fontFamily: {
+        firago: ['"FiraGO"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
