268d6ac7be1f95c58ae13e9775e7b96fa6650ea1
Marcin Gawski <marcin.gawski@gmail.com>
2024-06-04 13:33:25 +0200

    Persisting & restoring App DevTools open state.

--- src/app/testing/dev-tools/components/dev-tools/dev-tools.component.ts
+++ src/app/testing/dev-tools/components/dev-tools/dev-tools.component.ts
@@ -1,8 +1,15 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  effect,
+  inject,
+  signal,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingsIconComponent } from '@ui/icons/settings-icon';
 import { DevToolsHeaderComponent } from '@testing/dev-tools/components/dev-tools-header/dev-tools-header.component';
 import { DevToolsContentComponent } from '@testing/dev-tools/components/dev-tools-content/dev-tools-content.component';
+import { LocalStorageService } from '@core/storage';
 
 /**
  * This component is here only to make it easier to test whole application during verification by the Angular Training Team.
@@ -21,5 +28,16 @@ import { DevToolsContentComponent } from '@testing/dev-tools/components/dev-tool
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DevToolsComponent {
-  protected open = signal<boolean>(false);
+  private readonly OPEN_STATE_KEY: string = 'appDevToolsOpen';
+
+  protected storage = inject(LocalStorageService);
+  protected open = signal<boolean>(
+    this.storage.getItem<boolean>(this.OPEN_STATE_KEY) ?? false
+  );
+
+  public constructor() {
+    effect(() => {
+      this.storage.setItem(this.OPEN_STATE_KEY, this.open());
+    });
+  }
 }
