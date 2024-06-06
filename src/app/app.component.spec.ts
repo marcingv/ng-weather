d3da4abd9a3cc7b609ba8ae46e126be7b1c30a9a
Marcin Gawski <marcin.gawski@gmail.com>
2024-06-06 08:39:20 +0200

    DevTools: animating cache entries counter on change.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { provideRouter } from '@angular/router';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
 
 describe('TestDummyComponent', () => {
   let component: AppComponent;
@@ -9,7 +10,7 @@ describe('TestDummyComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
-      providers: [provideRouter([])],
+      providers: [provideRouter([]), provideNoopAnimations()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AppComponent);
